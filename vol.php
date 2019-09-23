c9d3acacc850c80032a93f6288fa4e11252c1ebc
Vlad <vlakos2895@gmail.com>
2019-09-23 14:58:16 +0300

    Начат Приволжский регион

--- vol.php
+++ vol.php
@@ -12,40 +12,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js"></script>
     <script type="text/javascript" src="js/raphael-min.js"></script>
     <script type="text/javascript" src="js/russian-map.js"></script>
-    <script>
-        // запрос на получение json регионов
-        window.onload = function () {
-            fetch('./js/data/volga_reg.json').then(function (response) {
-                response.json().then(function (data) {
-                    new RussianMap({
-                        viewPort: data.viewPort,
-                        mapId: 'volga_region',
-                        width: 1500,
-                        height: 1000,
-                        // дефолтовые атрибуты для контуров регионов
-                        defaultAttr: {
-                            fill: '#615dff', // цвет которым закрашивать
-                            stroke: '#ffffff', // цвет границы
-                            'stroke-width': 1, // ширина границы
-                            'stroke-linejoin': 'round' // скруглять углы
-                        },
-                        mouseMoveAttr: {
-                            fill: '#25669e'
-                        },
-                        /*onMouseMove: function (event) {
-                            console.log('mouse on ' + this.region.name);
-                        },
-                        onMouseOut: function (event) {
-                            console.log('out on ' + this.region.name);
-                        },*/
-                        onMouseClick: function (event) {
-                            console.log('clicked on ' + this.region.name);
-                        }
-                    }, data.regions);
-                });
-            });
-        };
-    </script>
     <title>Document</title>
 </head>
 <body>
@@ -61,56 +27,14 @@
 </div>
 <div class="col-md-12">
     <div class="row justify-content-center ">
-        <div id="volga_region">
+        <img src="img/vol.svg" alt="" id="img_full">
+    </div>
+    <div class="col-2">
+        <div class="row">
+            <canvas id="central_reg"></canvas>
         </div>
     </div>
 </div>
-<div class="col-2">
-    <div class="row">
-        <canvas id="central_reg"></canvas>
-    </div>
-</div>
-<div class="col-2">
-    <div class="row">
-        <canvas id="south_reg"></canvas>
-    </div>
-</div>
-<div class="col-2">
-    <div class="row">
-        <canvas id="caucasian_reg"></canvas>
-    </div>
-</div>
-<div class="col-2">
-    <div class="row">
-        <canvas id="crimea_reg"></canvas>
-    </div>
-</div>
-<div class="col-2">
-    <div class="row">
-        <canvas id="privolzh_reg"></canvas>
-    </div>
-</div>
-<div class="col-2">
-    <div class="row">
-        <canvas id="northwest_reg"></canvas>
-    </div>
-</div>
-<div class="col-3">
-    <div class="row">
-        <canvas id="ural_reg"></canvas>
-    </div>
-</div>
-<div class="col-4">
-    <div class="row">
-        <canvas id="siberia_reg"></canvas>
-    </div>
-</div>
-<div class="col-4">
-    <div class="row">
-        <canvas id="fareastern_reg"></canvas>
-    </div>
-</div>
-<object src="img/cr.svg"></object>
 </body>
-<!--<script src="js/charts.js"></script>-->
+<script src="js/charts/vol_charts.js"></script>
 </html>
